c53c05c95928a1e63f8ed5e4bd8396b543fa1e6e
archangel9800 <pain_s@mail.ru>
2016-09-15 19:35:36 +0300

    11

---
+++ dev/js/includes/2.1block3.js
@@ -0,0 +1,104 @@
+function forBlock3(){
+    if($('#block3').length > 0){
+//    font size
+    if ($('#block3').width() > 300 && $('#block3').width() <= 400  ){
+       $("#block3 .normal_font").css({
+       fontSize: $('#block3').width()/24 + "px", 
+    }); 
+         $("#block3 .large_font").css({
+       fontSize: $('#block3').width()/24 + "px", 
+    }); 
+        $("#block3 .very_large_font").css({
+       fontSize: $('#block3').width()/10 + "px", 
+    });  
+   };  
+    
+    if ($('#block3').width() > 400 && $('#block3').width() <= 500  ){
+     $("#block3 .normal_font").css({
+       fontSize: $('#block3').width()/30 + "px", 
+    }); 
+         $("#block3 .large_font").css({
+       fontSize: $('#block3').width()/25 + "px", 
+    }); 
+        $("#block3 .very_large_font").css({
+       fontSize: $('#block3').width()/10 + "px", 
+    });  
+   };  
+    
+     if ($('#block3').width() > 500 && $('#block3').width() <= 600  ){
+        $("#block3 .normal_font").css({
+       fontSize: $('#block3').width()/35 + "px", 
+    }); 
+         $("#block3 .large_font").css({
+       fontSize: $('#block3').width()/30 + "px", 
+    }); 
+        $("#block3 .very_large_font").css({
+       fontSize: $('#block3').width()/10 + "px", 
+    }); 
+   }; 
+    
+    if ($('#block3').width() > 600 && $('#block3').width() <= 700 ){
+    $("#block3 .normal_font").css({
+       fontSize: $('#block3').width()/40 + "px", 
+    }); 
+         $("#block3 .large_font").css({
+       fontSize: $('#block3').width()/30 + "px", 
+    }); 
+        $("#block3 .very_large_font").css({
+       fontSize: $('#block3').width()/15 + "px", 
+    }); 
+     };
+    
+     if ($('#block3').width() > 700 && $('#block3').width() <= 768  ){
+    $("#block3 .normal_font").css({
+       fontSize: $('#block3').width()/45 + "px", 
+    }); 
+         $("#block3 .large_font").css({
+       fontSize: $('#block3').width()/30 + "px", 
+    }); 
+        $("#block3 .very_large_font").css({
+       fontSize: $('#block3').width()/15 + "px", 
+    }); 
+     };
+    
+    if ( $('#block3').width() > 768 && $('#block3').width() <= 992 ){
+        $("#block3 .normal_font").css({
+       fontSize: $('#block3').width()/50 + "px", 
+    }); 
+         $("#block3 .large_font").css({
+       fontSize: $('#block3').width()/30 + "px", 
+    }); 
+        $("#block3 .very_large_font").css({
+       fontSize: $('#block3').width()/15 + "px", 
+    }); 
+   }; 
+    if ( $('#block3').width() > 992 ){
+        $("#block3 .normal_font").css({
+       fontSize: $('#block3').width()/60 + "px", 
+    }); 
+         $("#block3 .large_font").css({
+       fontSize: $('#block3').width()/40 + "px", 
+    }); 
+        $("#block3 .very_large_font").css({
+       fontSize: $('#block3').width()/20 + "px", 
+    }); 
+   }; 
+    
+        
+        
+        
+        
+
+        
+        
+
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
--- dev/js/includes/functionsStart.js
+++ dev/js/includes/functionsStart.js
@@ -4,15 +4,12 @@ $( document ).ready(function() {
     
     
     
-    
-    
-    
+
     
     
     
 setHeight(); // устанавливаем высоту окна при первой загрузке страницы
 $(window).resize( setHeight ); // обновляем при изменении размеров окна
-setHeight();
 
 
 
@@ -22,8 +19,24 @@ $(window).resize( forBlock1 );
     
 forBlock2();
 $(window).resize( forBlock2 );
-        
+    
 forBlock3();
 $(window).resize( forBlock3 );    
+        
+forBlock20();
+$(window).resize( forBlock20 );    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 });
